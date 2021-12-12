697ea9d7fbb56127dbd3fba5654485e456b9b897
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-12 11:41:35 -0500

    elif command testing

--- 01.Simple.sh
+++ 01.Simple.sh
@@ -2,6 +2,28 @@
 #pwd
 a=100
 
-echo a=${a}
+#echo a=${a}
 
-echo -e "\e[31m Hello world \e[m"
+#echo -e "\e[31m Hello world \e[m"
+
+read -p "enter your age" ; age
+
+if [ "${age}" -le 17 ]; then
+
+  echo "-e /e[31m you are a minor /e[m"
+
+elif [ "${age}" -ge 60 ]; then
+
+echo "-e /e[31m you are old citizen /e[m"
+
+else
+
+  echo "-e /e[31m you are a major /e[m"
+
+fi
+
+
+
+
+
+
